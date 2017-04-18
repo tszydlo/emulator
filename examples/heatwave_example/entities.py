9b003651ba996a5b2e40aced7487bf88a03aa25a
Joanna Sendorek <send.joanna@gmail.com>
2017-04-18 20:18:29 +0200

    Real-time simple simulation with plotting.

--- examples/heatwave_example/entities.py
+++ examples/heatwave_example/entities.py
@@ -1,60 +1,50 @@
 import collections
-
 import numpy as np
 
-class MeasurementVector():
 
+class MeasurementVector():
     def __init__(self):
         self.vector = collections.namedtuple("TemperatureVector", ['temperature'])
         self.vector.temperature = 0
 
 
-class Space():
-
+class Space(object):
     def __init__(self):
         self.length = 50
         self.width = 50
         self.points = [[MeasurementVector() for i in range(self.length)] for j in range(self.width)]
 
-
-def transformation(space, time):
-    dt = 0.1
-    # heating device
-    Gr = np.eye(10) * 2000
-    for iGr in range(10):
-        Gr[iGr, -iGr - 1] = 2000
-    for i in range(0, 10):
-        for j in range(0, 10):
-            point = space.points[i+20][j+10]
-            if Gr[i, j]:
-                point.vector.temperature = Gr[i, j]
-    for i in range(0, 10):
-        for j in range(0, 10):
-            point = space.points[i + 20][j + 30]
-            if Gr[i, j]:
-                point.vector.temperature = Gr[i, j]
-    T = np.arange(0, 10, dt)
-    MM = []
-    for i in range(len(T)):
-        for j in range(1, space.length - 1):
-            for i in range(1, space.width - 1):
-
-                space.points[i][j].vector.temperature = (space.points[i - 1][j].vector.temperature
-                                      + space.points[i + 1][j].vector.temperature
-                                      + space.points[i][j - 1].vector.temperature
-                                      + space.points[i][j + 1].vector.temperature) / 4
-
-        # Re-assert heaters
+    def transformation(self):
+        # heating device
+        gr = np.eye(10) * 2000
+        for iGr in range(10):
+            gr[iGr, -iGr - 1] = 2000
         for i in range(0, 10):
             for j in range(0, 10):
-                point = space.points[i + 20][j + 10]
-                if Gr[i, j]:
-                    point.vector.temperature = Gr[i, j]
+                point = self.points[i + 20][j + 10]
+                if gr[i, j]:
+                    point.vector.temperature = gr[i, j]
         for i in range(0, 10):
             for j in range(0, 10):
-                point = space.points[i + 20][j + 30]
-                if Gr[i, j]:
-                    point.vector.temperature = Gr[i, j]
+                point = self.points[i + 20][j + 30]
+                if gr[i, j]:
+                    point.vector.temperature = gr[i, j]
 
-        MM.append(space.points.copy())
-    return MM
+            for j in range(1, self.length - 1):
+                for i in range(1, self.width - 1):
+                    self.points[i][j].vector.temperature = (self.points[i - 1][j].vector.temperature
+                                                            + self.points[i + 1][j].vector.temperature
+                                                            + self.points[i][j - 1].vector.temperature
+                                                            + self.points[i][j + 1].vector.temperature) / 4
+
+            # Re-assert heaters
+            for i in range(0, 10):
+                for j in range(0, 10):
+                    point = self.points[i + 20][j + 10]
+                    if gr[i, j]:
+                        point.vector.temperature = gr[i, j]
+            for i in range(0, 10):
+                for j in range(0, 10):
+                    point = self.points[i + 20][j + 30]
+                    if gr[i, j]:
+                        point.vector.temperature = gr[i, j]
--- examples/heatwave_example/example.py
+++ examples/heatwave_example/example.py
@@ -1,26 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation
-
-from examples.heatwave_example.entities import Space, transformation
+from examples.heatwave_example.entities import Space
 
 space = Space()
-transformation_result = transformation(space, 1)
-
-MM = np.array([[[transformation_result[i][j][k].vector.temperature for k in range(space.width)]
-                for j in range(space.length)] for i in range(100)])
+space.transformation()
 
 fig = plt.figure()
-pcm = plt.pcolormesh(MM[0])
+pcm = plt.pcolormesh(np.array([[space.points[j][i].vector.temperature for i in range(space.length)]
+                               for j in range(space.width)]), cmap='jet')
 plt.colorbar()
 
 
-# # Function called to update the graphic
-# def step(i):
-#     if i >= len(MM):
-#         return
-#     pcm.set_array(MM[i].flatten())
-#     plt.draw()
-#
-# anim = FuncAnimation(fig, step, interval=50)
+def step(i):
+    space.transformation()
+    pcm.set_array(np.array([[space.points[j][k].vector.temperature for k in range(space.length)]
+                            for j in range(space.width)]).ravel())
+    plt.draw()
+
+
+anim = FuncAnimation(fig, step, interval=1000)
 plt.show()
